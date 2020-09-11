2b9a1fdcab7c7b462d5b395ef000a4a5a4f727be
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2020-09-11 16:03:36 +0800

    添加图标

--- src/components/Heard.vue
+++ src/components/Heard.vue
@@ -1,10 +1,51 @@
 <template>
-  <div>这里是头部组件</div>
+  <div class="main">
+    <div class="loge">
+      <img src="../assets/imgs/logo.jpg" alt />
+    </div>
+    <div class="title">
+      <p>首页</p>
+      <p>新闻</p>
+      <p>案例</p>
+      <p>商品</p>
+      <p>售后</p>
+      <p>联系</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style  scoped>
+.main {
+  display: flex;
+  justify-content: space-between;
+}
+.title {
+  width: 80%;
+  padding: 10px 10%;
+  background: #000;
+  font-size: 16px;
+  display: flex;
+  justify-content: flex-end;
+  color: #fff;
+}
+p {
+  margin-left: 20px;
+}
+p:hover {
+  cursor: pointer;
+  color: yellow;
+}
+.loge {
+  width: 18%;
+  padding: 10px 10%;
+  background: #000;
+  cursor: pointer;
+}
+.loge > img {
+  width: 100%;
+}
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,31 +1,33 @@
 <template>
   <div class="home">
     <Heard></Heard>
-    <el-menu
-      :default-active="activeIndex2"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-    >
-      <el-menu-item index="1">处理中心</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">我的工作台</template>
-        <el-menu-item index="2-1">选项1</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-      </el-submenu>
-      <el-menu-item index="3">
-        <a href="https://www.ele.me" target="_blank">订单管理</a>
-      </el-menu-item>
-    </el-menu>
-    <el-carousel indicator-position="outside">
-      <el-carousel-item v-for="(item,index) in list" :key="index">
-        <img :src="item.img" alt />
-      </el-carousel-item>
-    </el-carousel>
+    <div class="main">
+      <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+      >
+        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="3">
+          <a href="https://www.ele.me" target="_blank">订单管理</a>
+        </el-menu-item>
+      </el-menu>
+      <el-carousel indicator-position="outside">
+        <el-carousel-item v-for="(item,index) in list" :key="index">
+          <img :src="item.img" alt />
+        </el-carousel-item>
+      </el-carousel>
+    </div>
     <Footer></Footer>
   </div>
 </template>
