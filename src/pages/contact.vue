138994c19ea36242d312b377b783f91b5ef34d40
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2020-09-14 08:58:00 +0800

    联系我们组件

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
-    <Heard></Heard>这里是联系我们
+    <Heard></Heard>
+    <div class="main">
+      <el-row>
+        <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
+          <el-card :body-style="{ padding: '0px' }">
+            <img src="../assets/imgs/banner.jpg" class="image" />
+            <div style="padding: 14px;">
+              <span>好吃的汉堡</span>
+              <div class="bottom clearfix">
+                <time class="time">{{ currentDate }}</time>
+                <el-button type="text" class="button">操作按钮</el-button>
+              </div>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
+
     <Footer></Footer>
   </div>
 </template>
@@ -13,8 +30,42 @@ export default {
     Heard,
     Footer,
   },
+  data() {
+    return {
+      currentDate: new Date(),
+    };
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.time {
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
 </style>
