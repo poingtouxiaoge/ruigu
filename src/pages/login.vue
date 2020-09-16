119072a95979b3c8fdbea4245422fda7ce3a35cb
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2020-09-16 15:59:43 +0800

    登录页面静态完成

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -2,10 +2,10 @@
   <div class="login">
     <div class="box">
       <div class="title">
-        <div class="denglu">
+        <div class="denglu" @click="tologin1">
           登录
         </div>
-        <div class="zhuce">
+        <div class="zhuce" @click="tologin2">
           注册
         </div>
       </div>
@@ -25,11 +25,25 @@
               type="password"
             ></el-input>
           </el-form-item>
-          <el-form-item label="验证码">
-            <el-input v-model="formLabelAlign.type"></el-input>
+        </el-form>
+      </div>
+      <!-- 注册模块 -->
+      <div class="login_num" v-if="type == 2">
+        <el-form
+          :label-position="labelPosition"
+          label-width="120px"
+          :model="formLabelAlign"
+        >
+          <el-form-item label="手机号">
+            <el-input v-model="formLabelAlign.name"></el-input>
+          </el-form-item>
+          <el-form-item label="设置密码">
+            <el-input v-model="formLabelAlign.region" type="text"></el-input>
           </el-form-item>
         </el-form>
       </div>
+      <div class="btns" @click="toindex" v-if="type == 1">登录</div>
+      <div class="btns" @click="toindex1" v-if="type == 2">注册</div>
     </div>
   </div>
 </template>
@@ -42,10 +56,25 @@ export default {
       labelPosition: "right",
       formLabelAlign: {
         name: "",
-        region: "",
-        type: ""
+        region: ""
       }
     };
+  },
+  methods: {
+    tologin1() {
+      this.type = 1;
+    },
+    tologin2() {
+      this.type = 2;
+    },
+    toindex() {
+      alert("登录成功");
+      this.$router.push("/");
+    },
+    toindex1() {
+      alert("注册成功，赶快去登录吧！！");
+      this.type = 1;
+    }
   }
 };
 </script>
@@ -53,7 +82,7 @@ export default {
 <style scoped>
 .login {
   width: 35.142857rem;
-  /* height: 500px; */
+  height: 500px;
   padding: 10% 0;
   text-align: center;
   background: url("../assets/imgs/login_bg.jpg");
@@ -61,7 +90,7 @@ export default {
 .box {
   width: 600px;
   margin: 0 auto;
-
+  padding-bottom: 20px;
   background: #fff;
 }
 .title {
@@ -82,5 +111,20 @@ export default {
 }
 .login_num {
   padding: 20px 30px;
+}
+.btns {
+  padding: 5px 0;
+  width: 160px;
+  text-align: center;
+  background: skyblue;
+  color: #fff;
+  font-size: 14px;
+  border-radius: 200px;
+  margin-left: 200px;
+}
+.btns:hover {
+  background: pink;
+  color: #333;
+  cursor: pointer;
 }
 </style>
